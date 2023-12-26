acbd9ad7bed47c4df1b3fa07eb1d3a202a7a4d9a
manju7p <107907426+manju7p@users.noreply.github.com>
2023-12-26 22:33:38 +0530

    changes

--- strapi-projecct/config/server.js
+++ strapi-projecct/config/server.js
@@ -2,14 +2,9 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('29d33fae0bff833f07d361e8e2b890e61ad918ca517dece4f90e214e4137a2f484a4b0d2f9526d77c160a19c17fbd500a98c25f4b6eeb0370bbc5cfec6932306'),
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  middleware: {
-    session: {
-      secret: env('SESSION_SECRET', '29d33fae0bff833f07d361e8e2b890e61ad918ca517dece4f90e214e4137a2f484a4b0d2f9526d77c160a19c17fbd500a98c25f4b6eeb0370bbc5cfec6932306'),
-    },
   },
 });
